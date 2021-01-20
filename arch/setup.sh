031b47d0ef0c7e1b8eaf27ae566f5c8683c6922b
Christopher Poenaru <kiambogo@gmail.com>
2021-01-19 16:45:27 -0800

    arch install python

--- arch/setup.sh
+++ arch/setup.sh
@@ -25,6 +25,7 @@ echo "Installing core packages..."
 pacman -Sy --noconfirm \
      base-devel sudo acpi tlp light mesa vulkan-intel libxss xdg-utils gtk2 libcurl-gnutls openssh man htop \
      git vim sed awk kitty tmux neovim emacs \
+     python python-pip \
      sway swayidle swaylock waybar wofi xorg-xwayland dmenu grim slurp wl-clipboard imv \
      wget networkmanager curl blueman whois \
      alsa alsa-utils volumeicon pulseaudio pavucontrol \
