faf2aac8020b9d53966c1784d73c715fe59e76cf
Christopher Poenaru <kiambogo@gmail.com>
2021-01-19 17:16:17 -0800

    add vmd-mode to doom

--- doom/.doom.d/packages.el
+++ doom/.doom.d/packages.el
@@ -14,6 +14,8 @@
 ;; https://github.com/raxod502/straight.el#the-recipe-format
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
+(package! another-package
+  :recipe (:host github :repo "blak3mill3r/vmd-mode"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
