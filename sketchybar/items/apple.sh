e146085de89a49f43acff293021fbd565462fc6f
Christopher Poenaru <kiambogo@gmail.com>
2023-07-02 01:12:52 -0700

    add apple to bar

---
+++ sketchybar/items/apple.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+POPUP_OFF="sketchybar --set apple.logo popup.drawing=off"
+POPUP_CLICK_SCRIPT="sketchybar --set \$NAME popup.drawing=toggle"
+
+apple_logo=(
+  icon=􀣺
+  icon.font="$FONT:Bold:20.0"
+  icon.color=$ORANGE
+  padding_right=15
+  label.drawing=on
+  click_script="$POPUP_CLICK_SCRIPT"
+)
+
+apple_prefs=(
+  icon=$PREFERENCES
+  label="Preferences"
+  click_script="open -a 'System Preferences'; $POPUP_OFF"
+)
+
+apple_activity=(
+  icon=$ACTIVITY
+  label="Activity"
+  click_script="open -a 'Activity Monitor'; $POPUP_OFF"
+)
+
+apple_lock=(
+  icon=$LOCK
+  label="Lock Screen"
+  click_script="pmset displaysleepnow; $POPUP_OFF"
+)
+
+sketchybar --add item apple.logo left                  \
+           --set apple.logo "${apple_logo[@]}"         \
+                                                       \
+           --add item apple.prefs popup.apple.logo     \
+           --set apple.prefs "${apple_prefs[@]}"       \
+                                                       \
+           --add item apple.activity popup.apple.logo  \
+           --set apple.activity "${apple_activity[@]}" \
+                                                       \
+           --add item apple.lock popup.apple.logo      \
+           --set apple.lock "${apple_lock[@]}"
