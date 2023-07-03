837358346e19b9d863ad1420e039d57f218fc7b6
Christopher Poenaru <kiambogo@gmail.com>
2023-07-03 00:20:48 -0700

    gopls

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,7 +1,6 @@
 local plugins = {
-	{
-		"TimUntersberger/neogit"
-	},
+	{ 'nvim-lua/plenary.nvim' },
+	{ "TimUntersberger/neogit" },
 	{
 		'rmehri01/onenord.nvim',
 		lazy = false,
@@ -27,15 +26,69 @@ local plugins = {
 	{
 		'VonHeikemen/lsp-zero.nvim',
 		branch = 'v2.x',
+		lazy = true,
+		config = function()
+			-- This is where you modify the settings for lsp-zero
+			-- Note: autocompletion settings will not take effect
+
+			require('lsp-zero.settings').preset({})
+		end
+	},
+	{
+		'hrsh7th/nvim-cmp',
+		event = 'InsertEnter',
 		dependencies = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-		        -- Autocompletion
-		        {'hrsh7th/nvim-cmp'},     -- Required
-		        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-		        {'L3MON4D3/LuaSnip'},     -- Required
-	        },
-        },
+			{'L3MON4D3/LuaSnip'},
+		},
+		config = function()
+			-- Here is where you configure the autocompletion settings.
+			-- The arguments for .extend() have the same shape as `manage_nvim_cmp`: 
+			-- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/api-reference.md#manage_nvim_cmp
+
+			require('lsp-zero.cmp').extend()
+
+			-- And you can configure cmp even more, if you want to.
+			local cmp = require('cmp')
+			local cmp_action = require('lsp-zero.cmp').action() 
+
+			cmp.setup({
+				mapping = {
+					['<C-Space>'] = cmp.mapping.complete(),
+					['<C-f>'] = cmp_action.luasnip_jump_forward(),
+					['<C-b>'] = cmp_action.luasnip_jump_backward(),
+				}
+			})
+		end
+	},
+	{
+		'neovim/nvim-lspconfig',
+		cmd = 'LspInfo',
+		event = {'BufReadPre', 'BufNewFile'},
+		dependencies = {
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'williamboman/mason-lspconfig.nvim'},
+			{
+				'williamboman/mason.nvim',
+				build = function()
+					pcall(vim.cmd, 'MasonUpdate')
+				end,
+			},
+		},
+		config = function()
+			local lsp = require('lsp-zero')
+			lsp.on_attach(function(client, bufnr)
+				lsp.default_keymaps({buffer = bufnr})
+			end)
+
+			lsp.ensure_installed({
+				'gopls',
+			})
+
+			require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+			lsp.setup()
+		end
+	},
 	{
 		'windwp/nvim-autopairs',
 		event = "InsertEnter",
