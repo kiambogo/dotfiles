33a0b8569bff6f8751db843bf4252caddffe9b63
Christopher Poenaru <cpoenaru@abnormalsecurity.com>
2025-10-21 14:27:14 -0700

    hide doom emacs titlebar

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -14,6 +14,9 @@
 ;;      Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
 ;;      directory (for easy access to its source code).
 
+(add-to-list 'default-frame-alist '(undecorated . t))
+
+
 (doom! :input
        :completion
        (company +childframe)           ; the ultimate code completion backend
